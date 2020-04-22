0c4fdab385da2b87a429f81a4eda21ccb665ddd4
suprad-parashar <suprad.s.parashar@gmail.com>
2020-04-22 21:41:52 +0530

    Done With Profile Information Window

--- app/src/main/java/com/example/android/learn/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/android/learn/ui/profile/ProfileFragment.java
@@ -5,7 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,16 +16,25 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.android.learn.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Objects;
 
 public class ProfileFragment extends Fragment {
 
-    TextView nameTextView, emailTextView, typeTextView, pointsTextView, statusTextView;
-    ImageView dpImageView;
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+    private FirebaseUser user = auth.getCurrentUser();
 
-    String name, email, type, status;
-    int points;
+    private TextView nameTextView, emailTextView, typeTextView, pointsTextView, statusTextView;
+    private ImageView dpImageView;
+
+    ProgressBar wait;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -32,6 +43,35 @@ public class ProfileFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        wait = view.findViewById(R.id.wait_profile);
+        nameTextView = view.findViewById(R.id.profile_user_name);
+        emailTextView = view.findViewById(R.id.profile_user_email);
+        typeTextView = view.findViewById(R.id.profile_user_type);
+        pointsTextView = view.findViewById(R.id.profile_user_bp);
+        statusTextView = view.findViewById(R.id.profile_user_status);
 
+        nameTextView.setText(user.getDisplayName());
+        emailTextView.setText(user.getEmail());
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference reference = database.getReference().child("users").child(user.getUid()).child("data");
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String type = (String) dataSnapshot.child("type").getValue();
+                long points = (long) dataSnapshot.child("points").getValue();
+                String status = (String) dataSnapshot.child("status").getValue();
+                typeTextView.setText(type);
+                pointsTextView.setText(String.valueOf(points));
+                statusTextView.setText(status);
+                wait.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getContext(), "Firebase Profile Sucks", Toast.LENGTH_LONG).show();
+                wait.setVisibility(View.GONE);
+            }
+        });
     }
 }
