e71ec65ec6c56e0c6aa38ef8fc56ddf1167f5442
suprad-parashar <suprad.s.parashar@gmail.com>
2020-06-03 11:57:25 +0530

    Search Functionality Added to Learn Module

--- app/src/main/java/com/learn/android/fragments/learn/LearnFragment.java
+++ app/src/main/java/com/learn/android/fragments/learn/LearnFragment.java
@@ -1,13 +1,24 @@
 package com.learn.android.fragments.learn;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnectionWrapper;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,6 +28,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.learn.android.R;
 import com.learn.android.activities.learn.CourseViewActivity;
@@ -25,11 +37,20 @@ import com.learn.android.objects.TreeViewHolder;
 import com.unnamed.b.atv.model.TreeNode;
 import com.unnamed.b.atv.view.AndroidTreeView;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class LearnFragment extends Fragment {
 
 	//Declare UI Variables.
 	private LinearLayout learnListView;
 	private ProgressBar loading;
+	private EditText searchBar;
+	private ListView searchListView;
+	private ImageView backButton;
+
+	//Data Variable
+	private final ArrayList<String> searchTopics = new ArrayList<>();
 
 	//Initialise Firebase Variables.
 	private FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -41,18 +62,43 @@ public class LearnFragment extends Fragment {
 	}
 
 	@Override
-	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+	public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
 
 		//Initialise UI Variables.
-		learnListView = view.findViewById(R.id.list_learn);
+		learnListView = view.findViewById(R.id.tree_view_layout);
 		loading = view.findViewById(R.id.wait);
+		searchBar = view.findViewById(R.id.search_bar);
+		searchListView = view.findViewById(R.id.search_results_list_view);
+		backButton = view.findViewById(R.id.back_button);
+
+		searchListView.setVisibility(View.GONE);
+		backButton.setVisibility(View.GONE);
+		learnListView.setVisibility(View.VISIBLE);
 
 		//Create Main Root Node.
 		final TreeNode root = TreeNode.root();
 
+		searchListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				Intent intent = new Intent(requireActivity(), CourseViewActivity.class);
+				intent.putExtra("title", searchTopics.get(position));
+				startActivity(intent);
+			}
+		});
+
+		backButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				backButton.setVisibility(View.GONE);
+				searchListView.setVisibility(View.GONE);
+				learnListView.setVisibility(View.VISIBLE);
+			}
+		});
+
 		//Populate Courses.
-		TreeNode syllabus = new TreeNode("My Syllabus");
+		TreeNode syllabus = new TreeNode("My Syllabus").setViewHolder(new TreeViewHolder(requireContext(), learnListView, 1));
 		syllabus.setClickListener(new TreeNode.TreeNodeClickListener() {
 			@Override
 			public void onClick(TreeNode node, Object value) {
@@ -95,6 +141,49 @@ public class LearnFragment extends Fragment {
 			@Override
 			public void onCancelled(@NonNull DatabaseError databaseError) {
 				Log.e("ERROR", databaseError.toString());
+			}
+		});
+
+		//Add Search Functionality
+		searchBar.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+			@Override
+			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+				if (actionId == EditorInfo.IME_ACTION_DONE) {
+
+					//Hide Keyboard
+					View keyBoardView = requireActivity().getCurrentFocus();
+					if (keyBoardView != null) {
+						InputMethodManager methodManager = (InputMethodManager) requireContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+						assert methodManager != null;
+						methodManager.hideSoftInputFromWindow(keyBoardView.getWindowToken(), 0);
+					}
+
+					final String searchQuery = searchBar.getText().toString().trim().toLowerCase();
+					if (!searchQuery.equals("")) {
+						backButton.setVisibility(View.VISIBLE);
+						searchListView.setVisibility(View.VISIBLE);
+						learnListView.setVisibility(View.GONE);
+						loading.setVisibility(View.VISIBLE);
+						searchTopics.clear();
+
+						FirebaseDatabase.getInstance().getReference().child("links").addListenerForSingleValueEvent(new ValueEventListener() {
+							@Override
+							public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+								for (DataSnapshot snapshot : dataSnapshot.getChildren())
+									if (Objects.requireNonNull(snapshot.getKey()).toLowerCase().contains(searchQuery))
+										searchTopics.add(snapshot.getKey());
+								searchListView.setAdapter(new ArrayAdapter<>(requireContext(), android.R.layout.simple_list_item_1, searchTopics));
+								loading.setVisibility(View.GONE);
+							}
+
+							@Override
+							public void onCancelled(@NonNull DatabaseError databaseError) {
+
+							}
+						});
+					}
+				}
+				return true;
 			}
 		});
 	}
