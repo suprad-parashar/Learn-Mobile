ba0e6b4b72ef85e4aac4e3ea071fba260d600892
suprad-parashar <suprad.s.parashar@gmail.com>
2020-06-17 22:13:58 +0530

    Added Image Chooser in EditProfile. Saving uploads file to FirebaseStorage

--- app/src/main/java/com/learn/android/Learn.java
+++ app/src/main/java/com/learn/android/Learn.java
@@ -1,7 +1,10 @@
 package com.learn.android;
 
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.SharedPreferences;
+import android.os.Build;
 
 import androidx.appcompat.app.AppCompatDelegate;
 
@@ -17,9 +20,24 @@ public class Learn extends Application {
 	public void onCreate() {
 		super.onCreate();
 		FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+		createNotificationChannel("Upload", "Notifications on Upload Progress and Status", "LearnUpload");
 		SharedPreferences settings = getSharedPreferences("settings", MODE_PRIVATE);
 		if (!settings.contains("darkMode"))
 			settings.edit().putInt("darkMode", AppCompatDelegate.MODE_NIGHT_NO).apply();
 		isDark = settings.getInt("darkMode", AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+	}
+
+	/**
+	 * Create a Notification Channel
+	 */
+	private void createNotificationChannel(String name, String description, String channelId) {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+			int importance = NotificationManager.IMPORTANCE_DEFAULT;
+			NotificationChannel channel = new NotificationChannel(channelId, name, importance);
+			channel.setDescription(description);
+			NotificationManager notificationManager = getSystemService(NotificationManager.class);
+			assert notificationManager != null;
+			notificationManager.createNotificationChannel(channel);
+		}
 	}
 }
--- app/src/main/java/com/learn/android/fragments/profile/EditProfileFragment.java
+++ app/src/main/java/com/learn/android/fragments/profile/EditProfileFragment.java
@@ -1,6 +1,15 @@
 package com.learn.android.fragments.profile;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -13,11 +22,15 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
@@ -26,17 +39,38 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 import com.learn.android.R;
 import com.learn.android.activities.HomeActivity;
+import com.yalantis.ucrop.UCrop;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Objects;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.app.Activity.RESULT_OK;
+import static android.provider.MediaStore.ACTION_IMAGE_CAPTURE;
 
 /**
  * This class is responsible for editing the profile of the User.
  */
 public class EditProfileFragment extends Fragment {
 
+	private static final int RC_TAKE_PHOTO = 787;
+	private static final int RC_CHOOSE_IMAGE = 724;
+	private static final int RC_CROP_IMAGE = 725;
 	//Initialise Firebase Variables
 	private FirebaseAuth auth = FirebaseAuth.getInstance();
 	private FirebaseUser user = auth.getCurrentUser();
@@ -47,6 +81,11 @@ public class EditProfileFragment extends Fragment {
 	private EditText userNameEditText, collegeNameEditText;
 	private Spinner currentlyIn, stream, branch, boardUniversity, semester;
 	private TextView boardUniversityLabel;
+	CircleImageView profileImage;
+
+	//Initialise Data Variables.
+	Bitmap bitmap = null;
+	private Uri imageUri;
 
 	@Nullable
 	@Override
@@ -67,6 +106,7 @@ public class EditProfileFragment extends Fragment {
 		boardUniversity = view.findViewById(R.id.board_university);
 		boardUniversityLabel = view.findViewById(R.id.board_university_label);
 		semester = view.findViewById(R.id.view_profile_semester);
+		profileImage = view.findViewById(R.id.profile_image);
 
 		//Set Progressbar.
 		wait.setVisibility(View.VISIBLE);
@@ -123,6 +163,61 @@ public class EditProfileFragment extends Fragment {
 			}
 		});
 
+		//Handle Profile Image Change
+		profileImage.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Dexter.withContext(requireContext())
+						.withPermissions(Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+						.withListener(new MultiplePermissionsListener() {
+							@Override
+							public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+								if (multiplePermissionsReport.areAllPermissionsGranted()) {
+									AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+									builder.setTitle("Select Mode")
+											.setItems(new String[]{
+													"Take Photo",
+													"Choose From Gallery"
+											}, new DialogInterface.OnClickListener() {
+												@Override
+												public void onClick(DialogInterface dialog, int which) {
+													if (which == 0) {
+														Intent intent = new Intent(ACTION_IMAGE_CAPTURE);
+														startActivityForResult(intent, RC_TAKE_PHOTO);
+													} else {
+														Intent intent = new Intent();
+														intent.setType("image/*");
+														intent.setAction(Intent.ACTION_GET_CONTENT);
+														startActivityForResult(intent, RC_CHOOSE_IMAGE);
+													}
+												}
+											})
+											.setCancelable(true);
+									builder.create().show();
+								} else {
+									AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+									builder.setTitle("Permissions Required")
+											.setMessage("Storage and Camera Permissions are required for obtaining Image.")
+											.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+												@Override
+												public void onClick(DialogInterface dialog, int which) {
+													dialog.dismiss();
+												}
+											})
+											.setCancelable(false);
+									builder.create().show();
+								}
+							}
+
+							@Override
+							public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+								permissionToken.continuePermissionRequest();
+							}
+						}).check();
+			}
+		});
+
+		//Set Adapters
 		currentlyIn.setAdapter(new ArrayAdapter<>(requireContext(), R.layout.support_simple_spinner_dropdown_item, getResources().getStringArray(R.array.currently_in)));
 		currentlyIn.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 			@Override
@@ -177,9 +272,95 @@ public class EditProfileFragment extends Fragment {
 				reference.child("branch").setValue(branch.getSelectedItem().toString());
 				reference.child("board_university").setValue(boardUniversity.getSelectedItem().toString());
 				reference.child("semester").setValue(semester.getSelectedItem().toString());
+
+				if (bitmap == null) {
+					try {
+						bitmap = MediaStore.Images.Media.getBitmap(requireContext().getContentResolver(), imageUri);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+				ByteArrayOutputStream stream = new ByteArrayOutputStream();
+				bitmap.compress(Bitmap.CompressFormat.JPEG, 70, stream);
+				byte[] byteArray = stream.toByteArray();
+				FirebaseStorage storage = FirebaseStorage.getInstance();
+				StorageReference reference = storage.getReference().child("Profile Pictures").child(user.getUid() + ".jpg");
+				UploadTask task = reference.putBytes(byteArray);
+				task.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+					@Override
+					public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+						Toast.makeText(requireContext(), "Profile Picture Uploaded", Toast.LENGTH_SHORT).show();
+					}
+				}).addOnFailureListener(new OnFailureListener() {
+					@Override
+					public void onFailure(@NonNull Exception e) {
+						Log.e("UPLOAD", e.toString());
+						Toast.makeText(requireContext(), "Upload Failed", Toast.LENGTH_SHORT).show();
+					}
+				});
+
 				HomeActivity.navController.navigate(R.id.navigation_view_profile);
 		}
 		return true;
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+		if (requestCode == RC_TAKE_PHOTO && resultCode == RESULT_OK && data != null) {
+			bitmap = (Bitmap) Objects.requireNonNull(data.getExtras()).get("data");
+			profileImage.setImageBitmap(bitmap);
+		} else if (requestCode == RC_CHOOSE_IMAGE && resultCode == RESULT_OK && data != null) {
+			Uri uri = data.getData();
+			assert uri != null;
+			imageUri = Uri.fromFile(new File(requireContext().getCacheDir(), "IMG_" + System.currentTimeMillis()));
+			UCrop.of(uri, imageUri)
+					.withAspectRatio(1, 1)
+					.withMaxResultSize(512, 512) // any resolution you want
+					.start(requireActivity());
+			Glide.with(requireContext())
+					.load(imageUri)
+					.into(profileImage);
+//			ByteArrayOutputStream stream = new ByteArrayOutputStream();
+//			assert bitmap != null;
+//			bitmap.compress(Bitmap.CompressFormat.JPEG, 70, stream);
+//			byte[] byteArray = stream.toByteArray();
+//			FirebaseStorage storage = FirebaseStorage.getInstance();
+//			StorageReference reference = storage.getReference().child("Profile Pictures").child(user.getUid() + ".jpg");
+//			UploadTask task = reference.putBytes(byteArray);
+//			task.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//				@Override
+//				public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//					Toast.makeText(requireContext(), "Profile Picture Uploaded", Toast.LENGTH_SHORT).show();
+//				}
+//			}).addOnFailureListener(new OnFailureListener() {
+//				@Override
+//				public void onFailure(@NonNull Exception e) {
+//					Log.e("UPLOAD", e.toString());
+//					Toast.makeText(requireContext(), "Upload Failed", Toast.LENGTH_SHORT).show();
+//				}
+//			});
+		} else if (requestCode == RC_CROP_IMAGE) {
+			Toast.makeText(requireContext(), "HELLLO", Toast.LENGTH_LONG).show();
+//			assert data != null;
+//			Uri uri = UCrop.getOutput(data);
+//			Glide.with(requireContext())
+//					.load(uri)
+//					.into(profileImage);
+//			FirebaseStorage storage = FirebaseStorage.getInstance();
+//			StorageReference reference = storage.getReference().child("Profile Pictures").child(user.getUid() + ".jpg");
+//			UploadTask task = reference.putFile(destinationUri);
+//			task.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//				@Override
+//				public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//					Toast.makeText(requireContext(), "Profile Picture Uploaded", Toast.LENGTH_SHORT).show();
+//				}
+//			}).addOnFailureListener(new OnFailureListener() {
+//				@Override
+//				public void onFailure(@NonNull Exception e) {
+//					Toast.makeText(requireContext(), "Upload Failed", Toast.LENGTH_SHORT).show();
+//				}
+//			});
+		}
 	}
 
 	@Override
